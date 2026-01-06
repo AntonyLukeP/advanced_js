e7319fe44a906ea19961612dad2c1519e7b7a72c
Antony Luke <antonyluke001@gmail.com>
2026-01-06 22:17:53 +0530

    update points

--- Kanban-board/src/App.jsx
+++ Kanban-board/src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import TaskCard from "./components/TaskCard"
-import { tasks, statuses } from './data/task-data'
+import { tasks as initialTasks, statuses } from './data/task-data'
 
 function App() {
 
+  const [ tasks, setTasks ] = useState(initialTasks); 
   const columns = statuses.map((status)=>
   { 
     const tasksInColumn = tasks.filter((task)=>task.status === status);
@@ -11,16 +13,36 @@ function App() {
       tasks: tasksInColumn
     }
   })
+  const updatedTaskPoints = ((tasktoupdate,newpoints)=>{
+    if(newpoints < 0)
+    return;
+      setTasks((prev)=>
+          prev.map((task)=>
+            task.id === tasktoupdate.id?
+            {
+              ...task, points : newpoints
+            }
+          : task
+          )
+      )
+    
+  }
+  )
   return (
     <div className="flex w-screen divide-x ">
         { columns.map((column)=>
         (
            <div className="flex-1" >
+            <div className="flex justify-between text-3xl font-bold items-center mr-2" >  
             <h1 className="text-center font-bold p-4 uppercase text-2xl"  >{column.status}</h1>
-           {column.tasks.map((task)=>
+            <h6>{column.tasks.reduce((sum,task)=>sum+task.points,0)}</h6>
+            </div>
+            <div>
+              {column.tasks.map((task)=>
            (
-            <TaskCard task={task} />
+            <TaskCard task={task} updateTaskPoints={updatedTaskPoints} />
            ))}
+            </div>
            </div>
         )) }
     </div>
--- Kanban-board/src/components/TaskCard.jsx
+++ Kanban-board/src/components/TaskCard.jsx
@@ -4,7 +4,8 @@ import  high  from "../assets/high.png"
 import  medium  from "../assets/medium.png"
 import  low  from "../assets/low.png"
 
-const TaskCard = ({task} ) => {
+const TaskCard = ({task,updateTaskPoints} ) => {
+    const points = task.points
   return (
     <div className='border rounded-lg px-2 m-2 bg-gray-50' >
         <div className='text-base font-semibold py-2' >
@@ -20,9 +21,11 @@ const TaskCard = ({task} ) => {
             {task.priority === 'medium' && <img src={medium} alt="medium" className='inline w-8' /> }
             {task.priority === 'low' && <img src={low} alt="low" className='inline w-8' /> }
             </div>
-            <span>
-                {task.points}
-            </span>
+            <div className='flex gap-2 items-center' >
+                <button onClick={()=>updateTaskPoints(task,points-1)} >-</button>
+                <span className='font-bold' >{task.points}</span>
+                <button onClick={()=>updateTaskPoints(task,points+1)} >+</button>
+            </div>
         </div>
 
     </div>
