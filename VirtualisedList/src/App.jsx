60c63db5553341da5f78df58a12558d2c79b7b22
Antony Luke <antonyluke001@gmail.com>
2026-01-09 17:01:14 +0530

    virtualisedList math

--- VirtualisedList/src/App.jsx
+++ VirtualisedList/src/App.jsx
@@ -1,8 +1,8 @@
 import VirtualisedList from './components/VirtualisedList';
 
 const items = Array.from({ length: 10000 }).map((_, i) => ({
-  id: i,
-  name: `Item ${i}`
+  id: i+1,
+  name: `Item ${i+1}`
 }));
 
 function App() {
--- VirtualisedList/src/components/VirtualisedList.jsx
+++ VirtualisedList/src/components/VirtualisedList.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const itemHeight = 40;
+const veiwportHeight = 400;
 
 const VirtualisedList = ({items}) => {
+
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const startIndex = Math.floor(scrollTop/itemHeight);
+    const visibleItemCount = Math.ceil(veiwportHeight/itemHeight)+2;
+
+    const visibleItems = items.slice(startIndex, startIndex+visibleItemCount);
+
+    
+    const paddingTop = startIndex * itemHeight;
+    const paddingBottom =
+        (items.length - (startIndex + visibleItemCount)) * itemHeight;
+
   return (
     <div
-     className='  flex items-center h-screen justify-center '
+     className='  flex flex-col items-center h-screen justify-center '
     >
-        <div className=' h-[400px] w-[400px] overflow-y-auto border-4 border-gray-500 rounded-2xl' >
-            {items.map((item)=>(
+
+        <div className=' h-[400px] w-[400px] overflow-y-auto border-4
+         border-gray-500 rounded-2xl' 
+        onScroll={(e)=>setScrollTop(e.target.scrollTop)} 
+        >
+            
+        <div
+          style={{
+            paddingTop: `${paddingTop}px`,
+            paddingBottom: `${paddingBottom}px`,
+          }}
+        >
+             {visibleItems.map((item)=>(
             <div key={item.id}
-             className='border-b  text-center  h-[40px] p-[8px] '  >
+             className="border-b  text-center  h-[40px] p-[8px]"  >
                 {item.name}</div>
         ) )}
+
+           </div>
+
+
         </div>
     </div>
   )
